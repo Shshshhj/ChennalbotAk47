f662a1636e8f108818a615513be4966cde89d19e
Dke76 <156434263+Dke76@users.noreply.github.com>
2024-03-25 15:34:29 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -23,7 +23,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 bot = Client("bot",
-             bot_token= "6647821815:AAFkjfuusWSC0dfv1eGcAzj0Gn3sdBn83HQ",
+             bot_token= "7040230291:AAGC5uNZkZ3fMWemQhOqfuNnjp_tkKvGilk",
              api_id=20319884,
              api_hash= "637e3ba6357aa3ba2f3bf5742e0fd066")
 
