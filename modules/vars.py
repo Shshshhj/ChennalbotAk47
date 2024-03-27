3184cad4b71722eab026954bda146d63a425cbb5
Dke76 <156434263+Dke76@users.noreply.github.com>
2024-03-27 13:49:43 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
 api_id = "20319884"
 api_hash = "637e3ba6357aa3ba2f3bf5742e0fd066"
-bot_token = "6881026718:AAEK0hMOKTN76OwCbxulP42-G38Qx6cHtx4"
+bot_token = "7056328510:AAGpZpQx-DGe237q2grWB-Nz52613Qc0n34"
 
